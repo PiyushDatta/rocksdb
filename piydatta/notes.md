d3cea7febbebc32a2d2843546923c0e9e6405bfc
Piyush Datta <piyushdattaca@gmail.com>
2021-04-25 15:06:44 -0700

    Add setup files to private branch

---
+++ piydatta/notes.md
@@ -0,0 +1,10 @@
+# RockDB Notes
+
+## Commands
+
+1. Compile and run test_file.cc
+    - `cd /home/piydatta/Documents/rocksdb && make static_lib && cd piydatta && make all`
+
+## Links
+1. Setup/Installation/Compilation: https://github.com/facebook/rocksdb/blob/master/INSTALL.md
+2. Contributers guide: https://www.bookstack.cn/read/rocksdb-en/458d285f9fcf34ac.md
---
+++ piydatta/test_file.cc
@@ -0,0 +1,36 @@
+// Used to test
+
+#include <cstdio>
+#include <string>
+#include <iostream>
+
+#include "rocksdb/db.h"
+#include "rocksdb/slice.h"
+#include "rocksdb/options.h"
+
+using namespace ROCKSDB_NAMESPACE;
+
+#if defined(OS_WIN)
+std::string kDBPath = "C:\\Windows\\TEMP\\rocksdb_simple_example";
+#else
+std::string kDBPath = "/tmp/rocksdb_test_file";
+#endif
+
+int main() {
+  DB* db;
+  Options options;
+  // Optimize RocksDB. This is the easiest way to get RocksDB to perform well
+  options.IncreaseParallelism();
+  options.OptimizeLevelStyleCompaction();
+  // create the DB if it's not already present
+  options.create_if_missing = true;
+
+  // open DB
+  Status s = DB::Open(options, kDBPath, &db);
+  assert(s.ok());
+
+  delete db;
+  
+  std::cout << "this is the test file" << std::endl;
+  return 0;
+}
